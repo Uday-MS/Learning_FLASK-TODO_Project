3e48ef4078480ad57aef0aaa5cf48d65d41e11a9
Uday-MS <msudayuday@gmail.com>
2025-10-26 00:26:50 +0530

    second

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
+
+def create_app():
+     app=Flask(__name__)
+
+     app.config['SECRET KEY']='your-secret-key'
+     app.config['SQLALCHEMY_DATABASE_URI']='sqllite:///todo.db'
+     app.config['SQLALCHEMY_TRACK_MODIFICATION']=False
+
+     db.__init__(app)
+
+     from app.routes.auth import auth_bp
+     from app.routes.task import task_bp
+     app.register_blueprint(auth_bp)
+     app.register_blueprint(task_bp)
+
+     return app
